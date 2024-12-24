65efed6dfe86b09874ea711ad6c29908533b41c2
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2024-12-25 01:12:13 +0530

    studied request params and query

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,17 @@ import express, {Express,Request,Response} from 'express'
 const app:Express=express();
 const PORT=4000;
 
+app.get('/test',(req:Request,res:Response)=>{
+    const {name}=req.query;
+    const {age}=req.query;
+    res.send({"name": name,"age": age});
+})
+
+app.post('/test/:userId/:siblings',(req:Request,res:Response)=>{
+    const {userId,siblings}=req.params;
+    res.send({"userid":userId,"siblings":siblings});
+})
+
 app.use('/',(req:Request,res:Response)=>{
     res.send("bye bye bye");
 })
